b9dd0d9cbceab41356d5af6878e059692f1f06b8
Kevin Murray <kdmfoss@gmail.com>
2017-08-10 11:07:37 +1000

    Updates of python code

--- python/kmkm/_kmkm.pyx
+++ python/kmkm/_kmkm.pyx
@@ -1,4 +1,11 @@
 # distutils: language = c++
+#
+# Copyright (c) 2017 Kevin Murray <kdmfoss@gmail.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from libcpp.string cimport string
 from libcpp.vector cimport vector
 from libcpp cimport bool
@@ -8,38 +15,39 @@ import numpy as np
 
 cdef extern from "kmkm.hh" namespace "kmkm":
     cdef cppclass KmerCounter[T]:
-        KmerCounter (int ksize, size_t cvsize, bool canonical, size_t cbf_tables) except +
+        KmerCounter()
+        KmerCounter(const string &filename)
+        KmerCounter(int ksize, size_t cvsize, bool canonical, size_t cbf_tables) except +
         void consume_from(const string &filename) except +
         void consume(const string &sequence) except +
         vector[T] counts() except +
+        int k() except +
 
 ctypedef KmerCounter[uint8_t] KmerCounterU8
 
+cdef class PyKmerCounter:
+    cdef readonly int ksize
+    cdef readonly size_t cvsize
+    cdef KmerCounterU8 *ctr
 
-
-cdef class KmerCounterSet:
-    cdef int ksize
-    cdef size_t cvsize
-    cdef bool canonical
-    cdef size_t cbf_tables
-    cdef public list samples
-    cdef public np.ndarray global_array
-
-    def __init__(self, int ksize, int cvsize, bool canonical=True, int cbf_tables=0):
+    def __init__(self, int ksize, int cvsize, bool canonical=True,
+                 int cbf_tables=0):
         self.ksize = ksize
         self.cvsize = cvsize
         self.canonical = canonical
         self.cbf_tables = cbf_tables
-        self.global_array = np.zeros((0, cvsize), dtype=np.uint8)
-        self.samples = []
-
+        self.ctr = new KmerCounterU8(self.ksize, self.cvsize, self.canonical,
+                                     self.cbf_tables)
 
     def count_file(self, str filename):
-        cdef KmerCounterU8 *ctr = new KmerCounterU8(self.ksize, self.cvsize, self.canonical, self.cbf_tables)
-        ctr.consume_from(filename.encode("ascii"))
-        c = ctr.counts()
+        self.ctr.consume_from(filename.encode("ascii"))
+
+    def counts(self):
+        c = self.ctr.counts()
         n = np.asarray(<np.uint8_t[:c.size()]>c.data()).reshape((1, self.cvsize))
-        self.global_array = np.concatenate((self.global_array, n), axis=0)
-        self.samples.append(filename)
 
-
+    def load_file(self, str filename):
+        del self.ctr
+        self.ctr = new KmerCounterU8(filename.encode("ascii"))
+        self.ksize = self.ctr.k()
+        self.cvsize = self.ctr.counts().size()
