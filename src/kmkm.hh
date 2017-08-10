184b25e51eecf57bf1592fd7d6a080957e9375ed
Kevin Murray <kdmfoss@gmail.com>
2017-08-10 20:55:38 +1000

    Fix up license headers

--- src/kmkm.hh
+++ src/kmkm.hh
@@ -3,7 +3,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-//
+
 #ifndef KMKM_HH_WY0MH8N1
 #define KMKM_HH_WY0MH8N1
 
--- src/kmseq.hh
+++ src/kmseq.hh
@@ -1,4 +1,11 @@
 // A thin C++ wrapper around kseq.h
+//
+// Copyright (c) 2017 Kevin Murray <kdmfoss@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef KMSEQ_HH_T0ZK1CKV
 #define KMSEQ_HH_T0ZK1CKV
 
