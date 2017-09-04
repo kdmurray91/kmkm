563e4a5f70ecb7c71eed6d2a30f574ceef055402
Kevin Murray <kdmfoss@gmail.com>
2017-09-04 15:48:25 +1000

    cython fixes

--- python/setup.py
+++ python/setup.py
@@ -5,8 +5,10 @@ from Cython.Build import cythonize
 setup(
     name="kmkm",
     ext_modules=cythonize(Extension(
-        "kmkm/_kmkm",
+        "kmkm._kmkm",
         sources=["kmkm/_kmkm.pyx", ],
         include_dirs=["../src", "../src/ext"],
+        extra_compile_args=['-std=c++14', ],
+        libraries=['boost_serialization', 'boost_iostreams', 'z'],
         language="c++",)),
 )
