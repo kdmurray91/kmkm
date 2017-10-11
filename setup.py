e04189050fc3efb826ed35c242c82696f5791d77
Kevin Murray <kdmfoss@gmail.com>
2017-10-11 21:04:51 +1100

    Add numpy includes to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import numpy as np
 
 inst_deps = [
     'zarr',
@@ -13,10 +14,13 @@ setup(
     ext_modules=cythonize(Extension(
         "kmkm._kmkm",
         sources=["kmkm/_kmkm.pyx", ],
-        include_dirs=["src", "src/ext"],
+        include_dirs=["src", "src/ext", np.get_include()],
         extra_compile_args=['-std=c++14', ],
         libraries=['boost_serialization', 'boost_system', 'boost_filesystem',
                    'boost_iostreams', 'z'],
         language="c++",)),
     install_requires=inst_deps,
+    setup_requires=[
+        'numpy',
+    ],
 )
